d19e862e37dd82b749fb608fc047bca1c106c266
nabendu82 <nabendu.biswas@gmail.com>
2023-06-16 16:44:19 +0530

    Most done

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,36 @@
+'use client';
+import { useEffect, useState } from "react";
+import PromptList from "./PromptList";
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
+  const handleSearchChange = e => {}
+  const handleTagClick = tag => {}
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      setAllPosts(data);
+    }
+    fetchPosts();
+  },[])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <PromptList data={allPosts} handleTagClick={handleTagClick} />
+    </section>
   )
 }
 
