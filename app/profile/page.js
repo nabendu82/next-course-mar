54f3941891d40b4c6a8cce7bf624586ba35750b3
nabendu82 <nabendu.biswas@gmail.com>
2023-06-20 16:48:14 +0530

    Profile functionality

---
+++ app/profile/page.js
@@ -0,0 +1,38 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+const SelfProfile = () => {
+    const router = useRouter()
+    const { data: session } = useSession()
+    const [myPosts, setMyPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${session?.user.id}/posts`);
+            const data = await response.json();
+            setMyPosts(data)
+        }
+        if(session?.user.id) fetchPosts()
+    },[session?.user.id])
+
+    const handleEdit = post => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+    
+    const handleDelete = async () => {}
+
+    return (
+        <Profile 
+            name="My"
+            desc="Welcome to your personalized profile page."
+            data={myPosts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
+}
+
+export default SelfProfile
--- components/PromptCard.js
+++ components/PromptCard.js
@@ -5,8 +5,11 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick }) => {
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const [copied, setCopied] = useState("");
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -50,6 +53,12 @@ const PromptCard = ({ post, handleTagClick }) => {
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p className='font-inter text-sm blue_gradient cursor-pointer'
       onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>Edit</p>
+          <p className='font-inter text-sm yellow_gradient cursor-pointer' onClick={handleDelete}>Delete</p>
+        </div>
+      )}
     </div>
   )
 }
